0abf3d21b3fa8f27152a3521ae89bfdac9a3d5f2
LaRenzo <dlarenzo@outlook.com>
2024-03-06 15:54:59 -0700

    fixed login message

--- src/app/components/LoginCheckButton.jsx
+++ src/app/components/LoginCheckButton.jsx
@@ -1,13 +1,11 @@
 "use client";
-
 import React from "react";
 import Link from "next/link";
-import AllPosts from "./AllPosts";
+
 import { useToast } from "@chakra-ui/react";
 import useAuth from "../../hooks/useAuth";
-// import LoginCheckButton from "./LoginCheckButton";
 
-const Main = () => {
+const LoginCheckButton = () => {
   const toast = useToast();
   const { isLoggedIn, user } = useAuth();
 
@@ -20,20 +18,27 @@ const Main = () => {
         duration: 9000,
         isClosable: true,
       });
-      console.log("You are not logged in");
+      <Link
+        onClick={() => handleSignInBeforeCreatePost}
+        href="/"
+        className="mr-5 bg-yellow-600 px-10 py-2 rounded-full"
+      >
+        Create Post
+      </Link>;
+      // console.log("You are not logged in");
+    } else {
+      <Link
+        href="/create"
+        className="mr-5 bg-yellow-600 px-10 py-2 rounded-full"
+      >
+        Create Post
+      </Link>;
     }
-  };
-
-  return (
-    <div className="w-4/5 bg-slate-800 p-10 mx-auto">
-      <h1 className="text-center font-extrabold text-5xl mb-10">
-        Welcome to Post-Its
-      </h1>
-
+    return (
       <div className="flex justify-center mb-10">
         {!isLoggedIn ? (
           <Link
-            onClick={() => handleSignInBeforeCreatePost()}
+            onClick={() => handleSignInBeforeCreatePost}
             href="/"
             className="mr-5 bg-yellow-600 px-10 py-2 rounded-full"
           >
@@ -55,11 +60,37 @@ const Main = () => {
           Create Post
         </Link> */}
       </div>
-      <div className="">
-        <AllPosts />
-      </div>
+    );
+  };
+};
+
+export default LoginCheckButton;
+
+const handleSignInBeforeCreatePost = async () => {
+  if (!isLoggedIn) {
+    toast({
+      title: "You are not logged in",
+      description: "Please login to create a post",
+      status: "error",
+      duration: 9000,
+      isClosable: true,
+    });
+    // console.log("You are not logged in");
+  }
+  return (
+    <div>
+      {!isLoggedIn ? (
+        <Link href="/" className="mr-5 bg-yellow-600 px-10 py-2 rounded-full">
+          Create Post
+        </Link>
+      ) : (
+        <Link
+          href="/create"
+          className="mr-5 bg-yellow-600 px-10 py-2 rounded-full"
+        >
+          Create Post
+        </Link>
+      )}
     </div>
   );
 };
-
-export default Main;
